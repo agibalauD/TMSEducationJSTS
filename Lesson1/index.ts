a47384dda91d01558df5d3f8a1d47415d06a791a
Daniil Agibalau <d.a.agibalau@gmail.com>
2026-08-26 13:21:57 +0300

    Переименовал test1.ts в course.ts

--- Lesson1/index.ts
+++ Lesson1/index.ts
@@ -1,5 +1,5 @@
 import {name} from "./name";
 import {age} from "./age";
-import {course} from "./test1";
+import {course} from "./course";
 
 console.log(`My name is ${name}, I am ${age} years old and I am learning ${course}.`);

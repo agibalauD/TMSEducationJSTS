1d66e81b152bf70fa0d950e8a0521ac9c91ac6f2
AgibalauD <d.a.agibalau@gmail.com>
2026-09-03 21:05:38 +0300

    resolve task1

--- task1.ts
+++ task1.ts
@@ -1,8 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const name: string = "Александр";
 
 const text: string = `Я, ${name}, учу TYPESCRIPT`;
 
-//Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
+//Написать программу, которая выводит text в нижнем регистре с вашим именем
+//  если имя не совпадает, то заменить его
 
-const yourName: string = "Настя";
+const yourName: string = "Даниил";
+if (text.includes(yourName)) {
+  console.log(text.toLowerCase());
+} else {
+  const FirstNameIndex = text.indexOf(name);
+  const LastNameIndex = FirstNameIndex + name.length;
+  const LastTextAfterName = text.slice(LastNameIndex);
+  const newText: string = text.slice(0, FirstNameIndex).concat(yourName).concat(LastTextAfterName);
+  console.log(newText.toLowerCase());
+}

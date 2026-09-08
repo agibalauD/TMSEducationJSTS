375967e3cd4858ab4f44768b2a4afa1a09df7933
AgibalauD <d.a.agibalau@gmail.com>
2026-09-08 20:33:56 +0300

    Add Lesson 5 homework

---
+++ Lesson5/task1.ts
@@ -0,0 +1,10 @@
+// Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+console.log(SumNumbers(6));
+
+function SumNumbers(num: number) {
+  let result: number = 0;
+  for (let i = 0; i <= num; i++) {
+    result += i;
+  }
+  return result;
+}
